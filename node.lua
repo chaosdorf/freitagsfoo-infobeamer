88f4c1c73809daea69144243adb5e6c048e6534a
Niklas Sombert <niklas@ytvwld.de>
2018-07-14 18:28:06 +0200

    Just put a blue rect at the bottom.
    The rest of the screen stays pink for now.

--- node.lua
+++ node.lua
@@ -10,9 +10,15 @@ local cpmono = resource.load_font("CPMono_v07_Plain.otf")
 local usericon = resource.load_image("user.png")
 local plannericon = resource.load_image("planner.png")
 
+-- colored rectangle
+local bluerect = resource.create_colored_texture(0, 164/255, 183/255, 0.7)
+
 function node.render()
     -- background
-    gl.clear(0, 164/255, 183/255, 0.7)
+    gl.clear(246/255, 36/255, 118/255, 1)
+    
+    -- rectangle
+    bluerect:draw(0, 350, 1024, 600)
     
     computerfont:write(50, 400, "Freitagsfoo", 100, 246/255, 36/255, 118/255, 1)
     usericon:draw(600, 400, 645, 445)
