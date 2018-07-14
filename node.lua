3d74a6dc88c7c418a055de7900e6ba7cbe7527e4
Niklas Sombert <niklas@ytvwld.de>
2018-07-14 19:27:47 +0200

    main: Make the title screen move to the top and blank.

--- node.lua
+++ node.lua
@@ -1,9 +1,26 @@
 gl.setup(1024, 600)
 
+local ytrans = 0
+local atrans = 0
+local transition = true
+
+-- colored rectangle
+local bluerect = resource.create_colored_texture(0, 164/255, 183/255, 0.7)
+
 function node.render()
     -- background
     gl.clear(246/255, 36/255, 118/255, 1)
     
+    -- rectangle
+    bluerect:draw(0, 350 + ytrans, 1024, 600 + (ytrans*1.2), 2 - atrans)
     local title_screen = resource.render_child("title_screen")
-    title_screen:draw(0, 350, 1024, 600)
+    title_screen:draw(0, 350 + ytrans, 1024, 600 + (ytrans*1.2), 1 + atrans)
+    
+    if transition then
+        ytrans = ytrans - 8
+        atrans = atrans - 0.025
+        if ytrans <= -350 then
+            transition = false
+        end
+    end
 end
