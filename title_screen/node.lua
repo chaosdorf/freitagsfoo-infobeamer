dbaf4bc9a67607431bda56fdabbbec0b13076760
Niklas Sombert <niklas@ytvwld.de>
2018-07-20 10:28:51 +0200

    title_screen: Load data from JSON.

--- title_screen/node.lua
+++ title_screen/node.lua
@@ -1,5 +1,8 @@
 gl.setup(1024, 250)
 
+local json = require "json"
+local current_data
+
 -- title
 local computerfont = resource.load_font("Computerfont.ttf")
 
@@ -16,7 +19,10 @@ function node.render()
     
     computerfont:write(50, 50, "Freitagsfoo", 100, 246/255, 36/255, 118/255, 1)
     usericon:draw(600, 50, 645, 95)
-    cpmono:write(650, 50, "FIXME", 50, 1, 1, 1, 1)
+    cpmono:write(650, 50, current_data["host"], 50, 1, 1, 1, 1)
     plannericon:draw(600, 125, 645, 170)
-    cpmono:write(650, 125, "1970-01-01", 50, 1, 1, 1, 1, 1)
+    cpmono:write(650, 125, current_data["date"], 50, 1, 1, 1, 1, 1)
 end
+
+local content = resource.load_file("freitagsfoo.json")
+current_data = json.decode(content)
