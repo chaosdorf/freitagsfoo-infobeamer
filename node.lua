c322c42d9484e20e54fdb1ce4c97f38968351b9c
Niklas Sombert <niklas@ytvwld.de>
2018-07-20 22:07:27 +0200

    main: Add a slightly bumpy transition from talks to title.

--- node.lua
+++ node.lua
@@ -29,13 +29,30 @@ function node.render()
                 transition = false
                 ytrans = 0
                 screen = "talks"
+                start_time = sys.now()
             end
         end
     elseif screen == "talks" then
-        resource.render_child("talks_title"):draw(0, 0, 1024, 180)
+        resource.render_child("talks_title"):draw(0, 0 - ytrans, 1024, 180 - ytrans)
         resource.render_child("talks_screen"):draw(0, 180, 1024, 600, 1 + atrans)
-        if atrans < 0 then
+        if atrans < 0 and not transition then
             atrans = atrans + 0.025
+        end
+        
+        if sys.now() > start_time + 10 then
+            transition = true
+        end
+        
+        if transition then
+            ytrans = ytrans - 8
+            atrans = atrans - 0.025
+            if ytrans <= -350 then
+                transition = false
+                ytrans = 0
+                atrans = 0
+                screen = "title"
+                start_time = sys.now()
+            end
         end
     end
 end
