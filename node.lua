e3d176c08ddbd9ffea7f15d284fd44884366e157
Niklas Sombert <niklas@ytvwld.de>
2018-07-31 21:13:13 +0200

    Restructure main animation.

--- node.lua
+++ node.lua
@@ -13,47 +13,49 @@ function node.render()
     -- background
     gl.clear(246/255, 36/255, 118/255, 1)
     
-    if screen == "title" then
-        -- rectangle
-        resource.render_child("title_background"):draw(0, 0, 1024, 350, 1 + atrans)
-        resource.render_child("talks_title"):draw(0, 350 + ytrans, 1024, 600 + (ytrans*1.2), 0 - atrans)
-        resource.render_child("title_screen"):draw(0, 350 + ytrans, 1024, 600 + (ytrans*1.2), 1 + atrans)
+    if screen == "title" and not transition then
+        resource.render_child("title_background"):draw(0, 0, 1024, 350, 1)
+        resource.render_child("title_screen"):draw(0, 350, 1024, 600, 1)
         
         if sys.now() > start_time + 10 then
             transition = true
         end
+    elseif screen == "title" and transition then
+        resource.render_child("title_background"):draw(0, 0, 1024, 350, 1 + atrans)
+        resource.render_child("talks_title"):draw(0, 350 + ytrans, 1024, 600 + (ytrans*1.2), 0 - atrans)
+        resource.render_child("title_screen"):draw(0, 350 + ytrans, 1024, 600 + (ytrans*1.2), 1 + atrans)
         
-        if transition then
-            ytrans = ytrans - 8
-            atrans = atrans - 0.025
-            if ytrans <= -350 then
-                transition = false
-                ytrans = 0
-                screen = "talks"
-                start_time = sys.now()
-            end
+        ytrans = ytrans - 8
+        atrans = atrans - 0.025
+        if ytrans <= -350 then
+            transition = false
+            ytrans = 0
+            screen = "talks"
+            start_time = sys.now()
         end
-    elseif screen == "talks" then
-        resource.render_child("talks_title"):draw(0, 0 - ytrans, 1024, 180 - ytrans)
+    elseif screen == "talks" and not transition then
+        resource.render_child("talks_title"):draw(0, 0, 1024, 180, 1)
         resource.render_child("talks_screen"):draw(0, 180, 1024, 600, 1 + atrans)
-        if atrans < 0 and not transition then
+        
+        if atrans < 0 then
             atrans = atrans + 0.025
         end
         
         if sys.now() > start_time + 10 then
             transition = true
         end
+    elseif screen == "talks" and transition then
+        resource.render_child("talks_title"):draw(0, 0 - ytrans, 1024, 180 - ytrans)
+        resource.render_child("talks_screen"):draw(0, 180, 1024, 600, 1 + atrans)
         
-        if transition then
-            ytrans = ytrans - 8
-            atrans = atrans - 0.025
-            if ytrans <= -350 then
-                transition = false
-                ytrans = 0
-                atrans = 0
-                screen = "title"
-                start_time = sys.now()
-            end
+        ytrans = ytrans - 8
+        atrans = atrans - 0.025
+        if ytrans <= -350 then
+            transition = false
+            ytrans = 0
+            atrans = 0
+            screen = "title"
+            start_time = sys.now()
         end
     end
 end
