5514b7d111249a9bb890ef29a3e7f735ab0b9108
Niklas Sombert <niklas@ytvwld.de>
2019-10-04 12:00:26 +0200

    Fit more talks on the talks page

--- talks_screen/node.lua
+++ talks_screen/node.lua
@@ -1,4 +1,4 @@
-gl.setup(1024, 420)
+gl.setup(1024, 600)
 
 local json = require "json"
 -- This gets overwritten when the correct data has been fetched.
@@ -24,18 +24,18 @@ function node.render()
     local line_offset = 0
     
     for num, talk in pairs(current_data["talks"]) do
-        cpmono:write(40, (num+line_offset)*40, talk["title"], 32, 1, 1, 1, 1, 1)
+        cpmono:write(15, (num+line_offset)*40, talk["title"], 32, 1, 1, 1, 1, 1)
         
         for num_, person in pairs(talk["persons"]) do
             usericon:draw(700, (num+num_-1+line_offset)*39, 740, (num+num_-1+line_offset)*39+40)
-            cpmono:write(760, (num+num_-1+line_offset)*40, person, 32, 1, 1, 1, 1, 1)
+            cpmono:write(735, (num+num_-1+line_offset)*40, person, 32, 1, 1, 1, 1, 1)
             if num_ > 1 then
                 line_offset = line_offset + 1
             end
         end
     end
     
-    cpmono:write(20, 375, "Add yours at https://wiki.chaosdorf.de/18_Jahre_Chaosdorf_Lightningtalks", 21, 1, 1, 1, 1, 1)
+    cpmono:write(20, 570, "Add yours at https://wiki.chaosdorf.de/18_Jahre_Chaosdorf_Lightningtalks", 21, 1, 1, 1, 1, 1)
 end
 
 util.file_watch("freitagsfoo.json", function(content)
